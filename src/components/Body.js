a25135b423b8a203974826b894668599fab09c5e
Kundan Nigam <kundankumarnigam97@gmail.com>
2024-07-27 22:47:54 +0530

    Episode 6- useEffect to fetch actual API and use of Shimmar UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,29 @@
 import Search from "./Search";
 import RestaurantContainer from "./RestaurantContainer";
-import { useState } from "react";
-import resList from "../utils/restaurantList";
+import { useState, useEffect } from "react";
+// import resList from "../utils/restaurantList";
 import Filter from "./Filter";
+import Shimmer from "./Shimmer";
 
 
 const Body = () => {
-    const [listOfResto, setResListss] = useState(resList)
+    const [listOfResto, setResListss] = useState([])
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async() => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const result = await data.json();
+        setResListss(result?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+
+    if(listOfResto.length === 0) {
+        return <Shimmer/>
+    }
+
     return (
     <div className="body-container">
         <Search/>
