f862f275989b2905d84492dab8e7cce7682ff24b
Kundan Nigam <kundankumarnigam97@gmail.com>
2024-07-27 23:21:36 +0530

    Episode 6-Login Logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
-export const Header = () => (
-    <div className="heading">
-        <div className="logo-container">
-            <img src={LOGO_URL}/>
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact US</li>
-                <li>Cart</li>
-            </ul>
-        </div>
-    </div>
-);
+export const Header = () => {
+        const changeState = () => {
+            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+        }
 
+        const [btnName, setBtnName] = useState("Login");
+        return (
+            <div className="heading">
+                <div className="logo-container">
+                    <img src={LOGO_URL}/>
+                </div>
+                <div>
+                    <div className="nav-items">
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact US</li>
+                            <li>Cart</li>
+                        </ul>
+                        <button onClick={changeState}>{btnName}</button>
+                    </div>
+                    
+                </div>
+                
+                
+            </div>
+        );
+    }
 export default Header;
